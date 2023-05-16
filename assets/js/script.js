85efd677ae75c9d11a332afbac424821fd1db0b0
muhamadariefr <m.arief.r54321@gmail.com>
2023-05-16 13:08:06 +0700

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
 'use strict';
 
-/**
- * navbar variables
- */
-
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const header = document.querySelector("[data-header]");
 
-navToggleBtn.addEventListener("click", function () {
-  header.classList.toggle("active");
+navToggleBtn.addEventListener("click", function() {
+    header.classList.toggle("active");
 });
